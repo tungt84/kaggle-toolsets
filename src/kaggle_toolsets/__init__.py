1d6e02434973b7ce740657f645fd0833b4c9b433
tungt <tungt84@gmail.com>
2026-07-05 15:46:05 +0700

    add tpu

--- src/kaggle_toolsets/__init__.py
+++ src/kaggle_toolsets/__init__.py
@@ -2,6 +2,7 @@
 
 from .kaggle_toolsets import *
 from .langchain_toolsets import *
+from .tpu_toolset import *
 
 __all__ = [
     "__version__",
@@ -13,5 +14,7 @@ __all__ = [
     "suggest_vllm_gpu_config",
     "start_vllm_server",
     "install_langchain",
+    "start_vllm_with_tpu",
+    "suggest_vllm_tpu_config"
 ]
 __version__ = "0.1.0"
