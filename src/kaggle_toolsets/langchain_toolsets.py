4d9557aa3c446b341bd5a735d1e632d38fc22b6c
tungt <tungt84@gmail.com>
2026-07-06 14:50:20 +0700

    add lang graph

--- src/kaggle_toolsets/langchain_toolsets.py
+++ src/kaggle_toolsets/langchain_toolsets.py
@@ -5,3 +5,15 @@ def install_langchain():
     Install the langchain package using pip.
     """
     run_command("pip install langchain -q")
+
+def install_langgraph():
+    """
+    Install the langgraph package using pip.
+    """
+    run_command("pip install langgraph -q")
+
+def install_langgraph_openai():
+    """
+    Install the langgraph-openai package using pip.
+    """
+    run_command("pip install langchain-openai -q")
