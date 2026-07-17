025b41ab2065b512dea1a1ec808458124610a73d
Trần Tùng <tungt.20@grad.uit.edu.vn>
2026-07-17 10:07:54 +0700

    Update langchain_toolsets.py

--- src/kaggle_toolsets/langchain_toolsets.py
+++ src/kaggle_toolsets/langchain_toolsets.py
@@ -4,19 +4,19 @@ def install_langchain():
     """
     Install the langchain package using pip.
     """
-    run_command("pip install langchain langchain-core  -q")
+    run_command("pip install -U langchain langchain-core  -q")
 
 def install_langgraph():
     """
     Install the langgraph package using pip.
     """
-    run_command("pip install langgraph -q")
+    run_command("pip install -U langgraph -q")
 
 def install_langgraph_openai():
     """
     Install the langgraph-openai package using pip.
     """
-    run_command("pip install langchain-openai -q")
+    run_command("pip install -U langchain-openai -q")
 
 def install_langgraph_bundle():
     """
